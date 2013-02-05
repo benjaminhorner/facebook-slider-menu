3dfbb77701faaa78fb9d7501b620b4e58fb9ca34
Benjamin Horner <b.e.horner@gmail.com>
2013-02-05 16:17:45 +0100

    Updated snap function and removed toggleMenu() and snap() bugs
    Updated snap() function and added snapZone parametre.
    Updated toggleMenu() function
    Enabled resize() for desktops compatibility

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
         <head>
                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
                 <title></title>
+                <!-- CSS files -->
+                <link rel="stylesheet" href="css/facebookSliderMenu.css" type="text/css" media="screen" title="no title" charset="utf-8" />
                 <!-- JS files -->
                 <script type="text/javascript" src="js/jquery-1.9.0.js"></script>
-                <script type="text/javascript" src="js/bocetoSlider-1.0.0.js"></script>
-                <!-- CSS files -->
-                <link rel="stylesheet" href="css/bocetoSlider.css" type="text/css" media="screen" title="no title" charset="utf-8" />
+                <script type="text/javascript" src="js/jquery-easing.1.3.js"></script>
+                <script type="text/javascript" src="js/facebookSliderMenu-1.0.0.js"></script>
         </head>
         <body>
                 <!-- GLOBAL CONTAINER -->
-                <div id="bocetoSlider-container">
+                <div id="fbs-app-container">
                         <!-- BACKGROUND MENU -->
-                        <div id="bocetoSlider-menu-container">
-                                <div id="bocetoSlider-menu-header" class="header"></div>
-                                <div id="target">CLICK</div>
+                        <div id="fbs-menu-container">
+                                <div id="fbs-menu-header" class="fbs-header"></div>
                         </div>
                         <!-- ! BACKGROUND MENU -->
                         <!-- SITE CONTAINER -->
-                        <div id="bocetoSlider">
-                                <div class="header" id="bocetoSlider-header">
-                                        <a id="bocetoSlider-btn" href="#"></a>
+                        <div id="fbs-main-content-container">
+                                <div class="fbs-header" id="fbs-main-content-header">
+                                        <a id="fbs-menu-btn" href="#"></a>
                                 </div>
                                 2
                         </div>
@@ -30,11 +30,6 @@
                 <!-- ! GLOBAL CONTAINER -->
         </body>
         <script>
-                $("#bocetoSlider").bocetoSlider();
-                $('#target').toggle(function() {
-                        alert('First handler for .toggle() called.');
-                      }, function() {
-                        alert('Second handler for .toggle() called.');
-                      });
+                $("#fbs-main-content-container").facebookSliderMenu();
         </script>
 </html>
---
+++ js/facebookSliderMenu-1.0.0.js
@@ -0,0 +1,169 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function($) {
+        $.fn.facebookSliderMenu = function(params) {
+                /*
+                * facebookSliderMenu Object
+                * */
+                var $facebookSliderMenu = $(this);
+                /*
+                 * Object options set by user
+                 **/
+                var params = $.extend( {
+                        sliderWidth: '15', // %
+                        snapZoneWidth: '25', // %
+                        direction: 'left', // accepted values => left, both
+                        animationSpeed: 'fast',
+                        animationEasing: 'swing',
+                        menuBtn: '#fbs-menu-btn',
+                        open: true
+                }, params);
+                /*
+                * Variables
+                * */
+                var totalWidth = $(window).width();
+                var limitRight = Math.ceil((totalWidth)-((totalWidth*(params.sliderWidth/100))));
+                var doMove = false;
+                var limitLeft = 0;
+                var doSnap = false;
+                /*
+                 * Slider options
+                 **/
+                var $menuBtn = $facebookSliderMenu.find(params.menuBtn);
+                var sliderWidth = params.sliderWidth;
+                var openWidth = (100-sliderWidth);
+                var direction = params.direction;
+                var animationSpeed = params.animationSpeed;
+                var animationEasing = params.animationEasing;
+                var sliderWidthInPixels = Math.ceil((totalWidth)*(sliderWidth/100));
+                var snapZoneWidth = params.snapZoneWidth;
+                var snapRatio = Math.ceil(snapZoneWidth/sliderWidth);
+                var openOrClosed;
+                var open = params.open;
+                if(open) {openOrClosed = openWidth+'%'} else{openOrClosed = '0%'}
+                // Options for opening direction (left, right, both)
+                var optionsOpen;
+                var optionsClose;
+                if(direction == 'left') {
+                        optionsOpen = {left: limitLeft + 'px'};
+                        optionsClose = {left: limitRight + 'px'};
+                }
+                (open) ? $menuBtn.data('clickState', 1) : $menuBtn.data('clickState', 0);
+                /*
+                 * Re-initialize varaibels that need it
+                 **/
+                var reInit = function(){
+                        totalWidth = $(window).width();
+                        limitRight = Math.ceil((totalWidth)-((totalWidth*(params.sliderWidth/100))));
+                        optionsClose = {left: limitRight + 'px'};
+                        sliderWidthInPixels = Math.ceil((totalWidth)*(sliderWidth/100));
+                        var leftOffset = (totalWidth*0.85);
+                        if($facebookSliderMenu.offset().left != 0) $facebookSliderMenu.css('left', leftOffset);
+                        //console.log('totalWidth : '+totalWidth+' limitRight : '+limitRight+' sliderWidthInPixels : '+sliderWidthInPixels);
+                }
+                /*
+                 * Active Zone Object
+                 **/
+                var activeZone;
+                var getActiveZone = function(){
+                       return activeZone = ({
+                               left: $facebookSliderMenu.position().left,
+                               right: ($facebookSliderMenu.position().left)+sliderWidthInPixels
+                       })
+                }
+                /*
+                 * Snap Zone Object
+                 **/
+                var snapZone;
+                var getSnapZone = function(){
+                        return snapZone = ({
+                                left:  Math.ceil(sliderWidthInPixels*snapRatio),
+                                right: totalWidth-(Math.ceil(sliderWidthInPixels*snapRatio))
+                        })
+                }
+                /*
+                * Click and Move events
+                **/
+                var getStartClickEvent = function() {
+                       return ('touchstart' in window) ? 'touchstart' : 'mousedown';
+                };
+                var getEndClickEvent = function() {
+                       return ('touchend' in window) ? 'touchend' : 'mouseup';
+                };
+                var getMoveEvent = function() {
+                       return ('touchmove' in window) ? 'touchmove' : 'mousemove';
+                };
+                /*
+                 * Slider Left or Right
+                * */
+                $facebookSliderMenu.css(direction, openOrClosed)
+                /*
+                 * Slider Animation
+                 **/
+                var toggleMenu = function(){
+                        if($menuBtn.data('clickState')){
+                                $facebookSliderMenu.stop().animate(optionsOpen, animationSpeed, animationEasing);
+                        }
+                        else{
+                                $facebookSliderMenu.stop().animate(optionsClose, animationSpeed, animationEasing);
+                        }
+                        $menuBtn.data('clickState', !$menuBtn.data('clickState'));
+                }
+                /*
+                 * Glisse le slider en place quand on le lâche
+                 **/
+                var snapFBS = function(mouseX){
+                        getSnapZone();
+                        if($menuBtn.data('clickState')){
+                                (mouseX >= snapZone.right) ? $facebookSliderMenu.animate({left: limitRight}) : $facebookSliderMenu.animate({left: limitLeft});
+                        }
+                        else{
+                                (mouseX >= snapZone.left) ? $facebookSliderMenu.animate({left: limitRight}) : $facebookSliderMenu.animate({left: limitLeft});
+                        }
+                }
+                /*
+                * Déplacer le slider
+                * */
+                var moveSlider  = function(diff, mouseX){
+                        doSnap = true;
+                        var distToMove = (mouseX-diff);
+                        if(doMove) $facebookSliderMenu.offset({left: distToMove});
+                        if($facebookSliderMenu.offset().left <= limitLeft) {$facebookSliderMenu.offset({left: limitLeft}); $menuBtn.data('clickState', 0); doSnap = false;}
+                        if($facebookSliderMenu.offset().left >= limitRight) {$facebookSliderMenu.offset({left: limitRight}); $menuBtn.data('clickState', 1); doSnap = false;}
+                }
+                var followMouseX = function(diff){
+                        if(doMove){
+                                $(document).bind(getMoveEvent(), function(e){
+                                        var mouseX = e.pageX;
+                                        moveSlider(diff, mouseX);
+                                });
+                        }
+                 }
+                 $facebookSliderMenu.bind(getStartClickEvent(), function(e){
+                        var startMouseX = e.pageX;
+                        var initialOffset = $facebookSliderMenu.offset().left;
+                        var diff = (startMouseX-initialOffset);
+                        getActiveZone();
+                        if(startMouseX >= activeZone.left && startMouseX <= activeZone.right){
+                                doMove = true;
+                                followMouseX(diff);
+                        }
+                 });
+                 $facebookSliderMenu.bind(getEndClickEvent(), function(e){
+                         var mouseX = e.pageX;
+                         if(doSnap) snapFBS(mouseX);
+                         doMove = false;
+                 });
+                 /*
+                  * Toggle Slider : toggle() has been deprected as of jQuery 1.9.0 for anuthing but visibility so use data() instead
+                  **/
+                 $menuBtn.click(function(){
+                         toggleMenu();
+                 });
+                 $(window).resize(function(){
+                         reInit();
+                 })
+        };
+})(jQuery);
